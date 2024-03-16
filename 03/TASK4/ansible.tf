121aa6d74ecae9a02dca4b2dcb452acb53e48f1d
nerogen <tsgk@bm-it.dev>
2024-03-16 21:40:29 +0300

    Update ansible.tf

--- 03/TASK4/ansible.tf
+++ 03/TASK4/ansible.tf
@@ -1,19 +1,23 @@
+locals {
+  storage_instances = yandex_compute_instance.vm_storage[*]
+}
+
 resource "local_file" "hosts_templatefile" {
   content = templatefile("${path.module}/hosts.tftpl", {
-    webservers = length(yandex_compute_instance.web) > 0 ? [for w in yandex_compute_instance.web : {
+    webservers = [for w in yandex_compute_instance.web : {
       name = w.name,
       id = w.id,
       nat_ip_address = try(w.network_interface[0].nat_ip_address, "")
-    }] : [],
-    dbservers = length(yandex_compute_instance.db) > 0 ? [for d in yandex_compute_instance.db : {
+    }],
+    dbservers = [for d in yandex_compute_instance.db : {
       name = d.name,
       id = d.id,
       nat_ip_address = try(d.network_interface[0].nat_ip_address, "")
-    }] : [],
-    storages = [{
-      name = yandex_compute_instance.vm_storage.name,
-      id = yandex_compute_instance.vm_storage.id,
-      nat_ip_address = try(yandex_compute_instance.vm_storage.network_interface[0].nat_ip_address, "")
+    }],
+    storages = [for s in local.storage_instances : {
+      name = s.name,
+      id = s.id,
+      nat_ip_address = try(s.network_interface[0].nat_ip_address, "")
     }]
   })
   filename = "${abspath(path.module)}/inventory.ini"
